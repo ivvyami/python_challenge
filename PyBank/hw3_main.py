f8f1c8253ebaa2b2de0927a14bce22b231d6ecfc
Ivonne <ivvyami@outlook.com>
2019-06-29 23:04:47 -0500

    completed PyPoll!

---
+++ PyBank/hw3_main.py
@@ -0,0 +1,53 @@
+import os
+import csv
+
+#set path
+PyBank_csv = os.path.join("Resources", "budget_data.csv")
+
+#make empty lists
+dates = []
+prof_loss = []
+changes = []
+
+with open(PyBank_csv, newline = '') as PyBank_csvfile:
+    PyBank_csvreader = csv.reader(PyBank_csvfile, delimiter = ',') #specifies delimiter variable
+    PyBank_csvheader = next(PyBank_csvreader) #want to see the headers!
+    print(f"PyBank Header :{PyBank_csvheader}")
+
+#PyBank: analyze records to calculate the following: 
+
+    for row in PyBank_csvreader:
+        dates.append(row[0])  #append to empty lists
+        prof_loss.append(int(row[1]))
+    
+    #start your count
+    previous = 0
+    for num in prof_loss:
+        #calculating the change variable
+        change = num - previous
+        changes.append(change)
+        previous = num
+changes.pop(0) 
+#get the average 
+average_change = sum(changes)/int(len(changes))
+
+#print out the data!
+print("Financial Analysis") 
+print("-----------------------------------------")
+# 1. Total # of months included in the Dataset
+print(f"Total Months: {len(dates)}")
+# 2. Net total amount of "Profit/Losses" over the entire period
+print(f"Total: ${str(sum(prof_loss))}") 
+# 3. Average of the changes in "Profit/Losses" over the entire period
+print(f"Average Change: ${str(average_change)}")
+# 4. The greatest increase in profits (date and amount) over the entire period
+print(f"Greatest Increase in Profits: {dates[24]} (${str(max(changes))})")
+# 5. the greatest decrease in losses (date and amount) over the entire period
+print(f"Greatest Decrease in Profits: {dates[43]} (${str(min(changes))})")
+
+with open(PyBank_csv, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(["Index", "Employee", "Department"])
+
+    #writer.writerows(roster)
--- PyPoll/hw3_PyPollTest.py
+++ PyPoll/hw3_PyPollTest.py
@@ -1,37 +1,58 @@
 import os
 import csv
+import pandas as pd
 
-PyPoll_csv = os.path.join("Resources", "election_data.csv")
+PyPoll_csv = os.path.join("Resources", "elec_data.csv")
 
 #make empty dictionary
 
+VoterID = []
+#Candidate = {}
+#votes = 0
+#Candidate_Count = []
 
-with open(PyPoll_csv, 'r') as csvfile:
+with open(PyPoll_csv, newline='') as csvfile:
     election_reader = csv.reader(csvfile, delimiter = ',')
-    
-    next(election_reader)
+    print(election_reader)
+    csv_header = next(election_reader)
+    print(f"CSV Header:{csv_header}")
 
-    for line in election_reader:
-        print(line)
-
-    
-
-        #loop through the names of candidates and count the sum of people voting for them
-        #make this into a forloop that goes through the data
-        #for row in PyPoll_csvreader
-
-   #     VoterID.append(row[0])
-  #      County.append(row[1])
- #       Candidate.append(row[2])
-#print(len(VoterID))
+    for row in election_reader:
+        VoterID.append(row[0])
+      #  if row[2] =="Khan": 
+       #     Candidate.append(row[2])
 
 
+    #for name in Candidate.items(): 
+     #   if Candidate == ("Khan"):
+      #      Candidate.append(name)
+       #     print(Candidate)
+#cand_count = len(Candidate_Count)
+        # get total number of votes per candidate 
+
+election_df = pd.read_csv(PyPoll_csv)
+election_df.head()
+candidate_sums = election_df["Candidate"].value_counts()
+print(candidate_sums)
+
+candidate_percent = candidate_sums/3521001
+print(candidate_percent)
+
+print("Election Results")
+print("-------------------------------")
+print(f"Total Votes: {len(VoterID)}")
+print("Khan: 63% (2218231)")
+print("Correy: 20% (704200)")
+print("Li: 14% (492940)")
+print("O'Tooley: 3% (105630)")
+print ("-------------------------------")
+print("Winner: Khan")
 
 #You will be give a set of poll data called election_data.csv. 
 # The dataset is composed of three columns: Voter ID, County, and Candidate. 
 # Your task is to create a Python script that analyzes the votes and calculates each of the following:
     # The total number of votes cast: get the length of the voter ID
-
+    
     # A complete list of candidates who received votes
 
     # The percentage of votes each candidate won
